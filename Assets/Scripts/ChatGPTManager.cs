90457ab520602169be39d16b4591280ae3b784e2
Cris Chong <81703600+tmseldon@users.noreply.github.com>
2023-05-13 20:12:19 -0400

    Establishing connection with API

--- Assets/Scripts/ChatGPTManager.cs
+++ Assets/Scripts/ChatGPTManager.cs
@@ -1,20 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using OpenAI;
+using System.Threading.Tasks;
 
 namespace Game.AI
 {
     public class ChatGPTManager : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        /// <summary>
+        /// Route to the key for OpenAI
+        /// </summary>
+        private const string DIRECTORY = "D:/Repository/Assets/ChatGPT/Keys";
 
+        /// <summary>
+        /// Reference for the connection with ChatGPT
+        /// </summary>
+        private OpenAIClient _connectionApi;
+
+
+        private void Awake()
+        {
+            _connectionApi = new OpenAIClient(OpenAIAuthentication.LoadFromDirectory(DIRECTORY));
+            
+            if(_connectionApi == null)
+            {
+                Debug.LogError("Connection cannot be established with ChatGPT");
+                return;
+            }
+
+            // [TESTING] Getting list of models
+            //Task.Run(() => 
+            //{ 
+            //    GetModelsAsync(); 
+            //});
         }
 
-        // Update is called once per frame
-        void Update()
+
+        /// <summary>
+        /// Get list of available models
+        /// </summary>
+        /// <returns></returns>
+        private async Task GetModelsAsync() 
         {
+            var models = await _connectionApi.ModelsEndpoint.GetModelsAsync();
+
+            foreach (var model in models)
+            {
+                Debug.Log(model.ToString());
+            }
 
         }
     }
